25fa14fe012ff7fcd5abb0a8bfc58205965afc48
MindofJeremy <jnrsaurus@gmail.com>
2022-11-12 15:22:10 -0500

    commit

---
+++ Assets/scripts/battleScripts/monsterInfo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterInfo : MonoBehaviour
+{
+
+    public float health;
+    public float attack;
+
+    public float chargeMax;
+    public float defense;
+
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
